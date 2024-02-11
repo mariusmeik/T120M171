fea099690bf2849fbb15c9c18f2d06e4c410d428
DESKTOP-9PHC34C\P-Mac <mariusmeik@gmail.com>
2024-02-11 20:00:32 +0200

    Dabar žemiausi taškai gali buti keli, o ne vienas

--- ProgramosKodas.py
+++ ProgramosKodas.py
@@ -9,7 +9,8 @@ def find_highest_support(heights):
 
 def find_lowest_supports(heights):
     # Grąžina žemiausių atramų numerius (indeksus)
-    return heights.index(min(heights))
+    lowest_support = min(heights)
+    return  [index for index, height in enumerate(heights) if height == lowest_support]
 
 def find_longest_rise(heights):
     # Suranda ilgiausią pakilimą ir grąžina jo pradžios atramos numerį bei atramų skaičių
@@ -68,6 +69,8 @@ print(f"Peaks at supports numbers: {peaks}")
 # Grafiko braižymas
 plt.figure(figsize=(10, 6))
 plt.plot(h1, label="Height", marker='o')
+plt.plot([start_of_ascend, start_of_ascend + 1], [h1[start_of_ascend], h1[start_of_ascend + 1]], label=f"Steepest Ascend = {ascend_height}",marker='o', linestyle='-', linewidth=2, color='magenta')
+plt.plot([start_of_descend, start_of_descend + 1], [h1[start_of_descend], h1[start_of_descend + 1]], label=f"Steepes Descent = {descend_height}",marker='o', linestyle='-', linewidth=2, color='cyan')
 
 plt.scatter(peaks, [h1[i] for i in peaks], color='red', label="Peaks", marker='+', zorder=4)
 
@@ -76,9 +79,6 @@ plt.scatter(longest_rise_start, h1[longest_rise_start], color='black', marker='x
 plt.scatter(range(longest_rise_start, longest_rise_start + longest_rise_length), 
             [h1[i] for i in range(longest_rise_start, longest_rise_start + longest_rise_length)], 
             color='yellow', s=50, label="Longest Rise", zorder=2)
-
-plt.plot([start_of_ascend, start_of_ascend + 1], [h1[start_of_ascend], h1[start_of_ascend + 1]], label=f"Steepest Ascend = {ascend_height}",marker='o', linestyle='-', linewidth=2, color='magenta')
-plt.plot([start_of_descend, start_of_descend + 1], [h1[start_of_descend], h1[start_of_descend + 1]], label=f"Steepes Descent = {descend_height}",marker='o', linestyle='-', linewidth=2, color='cyan')
 
 plt.title("American Roller Coaster Supports")
 plt.xlabel("Support Number")
